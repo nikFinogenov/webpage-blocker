f4c7defe2035659468ea804c0a5a44c8a9e91bc4
Nikita Finogenov <nikita.finogenov@gmail.com>
2024-08-20 23:07:11 +0300

    trim link

--- popup.js
+++ popup.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // http://www.vsemba.sk/Portals/0/Subory/ERASMUS/IS_BE.pdf?ver=2023-11-20-144223-543
   // http://3porno365.biz/
   // http://mob.porno365.bond/
-  // https://y.hentaichan.live/user/N1kasik/
+  // https://y.hentaichan.live/user/
   // https://lms.khpi.ucode-connect.study/
   // https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0
   function trimLink(link) {
