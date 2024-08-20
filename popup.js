f9718945ebd70cc7e06a4134e78397acce80a33b
Nikita Finogenov <nikita.finogenov@gmail.com>
2024-08-20 15:56:10 +0300

    hotfix

--- popup.js
+++ popup.js
@@ -1,31 +1,57 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const linkInput = document.getElementById('linkInput');
-    const saveLink = document.getElementById('saveLink');
-    const linkList = document.getElementById('linkList');
-  
-    chrome.storage.sync.get(['links'], function(result) {
+  const linkInput = document.getElementById('linkInput');
+  const saveLink = document.getElementById('saveLink');
+  const deleteLast = document.getElementById('deleteLast');
+  const deleteAll = document.getElementById('deleteAll');
+  const linkList = document.getElementById('linkList');
+
+  chrome.storage.local.get(['links'], function(result) {
+    const links = result.links || [];
+    links.forEach(link => addLinkToList(link));
+  });
+
+  saveLink.addEventListener('click', function () {
+    const link = linkInput.value;
+    if (link) {
+      chrome.storage.local.get(['links'], function(result) {
+        const links = result.links || [];
+        links.push(link);
+        chrome.storage.local.set({ links: links }, function() {
+          addLinkToList(link);
+          linkInput.value = '';
+        });
+      });
+    }
+  });
+
+  deleteLast.addEventListener('click', function () {
+    chrome.storage.local.get(['links'], function(result) {
       const links = result.links || [];
-      links.forEach(link => addLinkToList(link));
-    });
-  
-    saveLink.addEventListener('click', function () {
-      const link = linkInput.value;
-      if (link) {
-        chrome.storage.sync.get(['links'], function(result) {
-          const links = result.links || [];
-          links.push(link);
-          chrome.storage.sync.set({ links: links }, function() {
-            addLinkToList(link);
-            linkInput.value = '';
-          });
+      if (links.length > 0) {
+        links.pop();
+        chrome.storage.local.set({ links: links }, function() {
+          refreshLinkList(links);
         });
       }
     });
-  
-    function addLinkToList(link) {
-      const li = document.createElement('li');
-      li.textContent = link;
-      linkList.appendChild(li);
-    }
   });
-  
+
+  deleteAll.addEventListener('click', function () {
+    chrome.storage.local.set({ links: [] }, function() {
+      const links = result.links || [];
+      refreshLinkList(links);
+    });
+  });
+
+  function addLinkToList(link) {
+
+    const li = document.createElement('li');
+    li.textContent = link;
+    linkList.appendChild(li);
+  }
+
+  function refreshLinkList(links) {
+    linkList.innerHTML = '';
+    links.forEach(link => addLinkToList(link));
+  }
+});
